046eb86fada0ca6a9073610b26698779e3e7da9e
JeongSu <zz1996zz@naver.com>
2021-06-03 20:43:32 +0900

    write test code

---
+++ todo-project/src/test/java/org/example/service/TodoServiceTest.java
@@ -0,0 +1,83 @@
+package org.example.service;
+
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
+import org.example.repository.TodoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceTest {
+    // 목을 사용하는 이유
+    // 1. 외부시스템에 의존하지 않고 자체테스트를 실행할 수 있어야하기 때문,
+    // 유닛테스트는 네트워크, 데이터베이스를 사용할 수 없다고 해서 유닛테스트도 사용 못하면 안되기때문이다.
+    // 2. 실제 데이터베이스를 활용하여 테스트를 하면, 데이터베이스안에 저장되어있는 민감한 데이터나 서비스중인 데이터 값들이
+    // 변경될 수 있기때문에 목을 사용하는 것이다.
+
+    @Mock
+    private TodoRepository todoRepository;
+
+    @InjectMocks
+    private TodoService todoService;
+
+    @Test
+    void add() {
+        when(this.todoRepository.save(any(TodoEntity.class)))
+                .then(AdditionalAnswers.returnsFirstArg());
+
+        TodoRequest expected = new TodoRequest();
+        expected.setTitle(("Test Title"));
+
+        TodoEntity actual = this.todoService.add(expected);
+
+        assertEquals(expected.getTitle(), actual.getTitle());
+    }
+
+    @Test
+    void searchById() {
+        TodoEntity entity = new TodoEntity();
+
+        entity.setId(123L);
+        entity.setTitle("TITLE");
+        entity.setOrder(0L);
+        entity.setCompleted(false);
+
+        Optional<TodoEntity> optional = Optional.of(entity);
+
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(optional);
+
+        TodoEntity actual = this.todoService.searchById(123L);
+
+        TodoEntity expected = optional.get();
+
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getTitle(), actual.getTitle());
+        assertEquals(expected.getOrder(), actual.getOrder());
+        assertEquals(expected.getCompleted(), actual.getCompleted());
+    }
+
+    @Test
+    public void searchByIdFailed() {
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> {
+            this.todoService.searchById(123L);
+        });
+    }
+}
